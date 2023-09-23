760e9d36fdd13c8ecacfdd42a42663da6bc731f0
Gioeleramondetti5 <gioeleramondetti20@gmail.com>
2023-09-23 16:10:55 +0200

    es protocol

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
-use tract_onnx::prelude::*;
+
+
+use std::fs::File;
+use tract_onnx::{prelude::*, pb::ModelProto};
+use std::io::Read;
 
 fn main() -> TractResult<()> {
+    tryprotoc();
     let model = tract_onnx::onnx()
         // load the model
         // esempio squeeze
@@ -8,7 +13,7 @@ fn main() -> TractResult<()> {
         // specify input type and shape
         .with_input_fact(0, InferenceFact::dt_shape(f32::datum_type(), tvec!(1, 3, 224, 224)))?;
         // optimize the model
-        //.into_optimized()?;
+        //.into_optimized()?; // migliore visualizzazione dei dati
         // make the model runnable and fix its inputs and outputs
 
     model.nodes.iter().for_each(|x|{
@@ -25,12 +30,40 @@ fn main() -> TractResult<()> {
         (resized[(x as _, y as _)][c] as f32 / 255.0 - mean) / std
     })
         .into();
-    let runModel = model.into_runnable()?;
+    let run_model = model.into_runnable()?;
     // run the model on the input
-    let result = runModel.run(tvec!(image))?;
+    let result = run_model.run(tvec!(image.into()))?;
 
     println!("result: {:?}", result);
     Ok(())
+}
+
+
+fn tryprotoc() {
+    let mut model_buffer = Vec::new();
+    let model_file_path = "src/squeezenet.onnx";
+
+    let mut model_file = File::open(model_file_path).expect("Failed to open the model file");
+    model_file
+        .read_to_end(&mut model_buffer)
+        .expect("Failed to read the model file");
+
+    // Parse the serialized ONNX model data
+    let mut model_proto = ModelProto::default();
+    model_proto
+        .merge(&model_buffer)
+        .expect("Failed to decode the model");
+
+    // Now you can work with the loaded ModelProto
+    println!("Model name: {:?}", model_proto.name);
+    println!(
+        "Number of inputs: {:?}",
+        model_proto.graph.as_ref().unwrap().input.len()
+    );
+    println!(
+        "Number of outputs: {:?}",
+        model_proto.graph.as_ref().unwrap().output.len()
+    );
 }
 
 
