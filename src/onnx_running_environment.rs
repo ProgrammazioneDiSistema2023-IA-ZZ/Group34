da97fc4a961e8a79f54a5cfab8f9814f7691e102
DarioMarchitelli <d.marchitelli1@studenti.poliba.it>
2023-11-23 11:24:30 +0100

    Fix last node not working

--- src/onnx_running_environment.rs
+++ src/onnx_running_environment.rs
@@ -36,27 +36,26 @@ impl OnnxRunningEnvironment {
         let graph = model.clone().graph.unwrap();
         //println!("{?}",graph.unwrap());
         let mut input_senders: Vec<Sender<TensorProto>> = Vec::new();
-        let mut output_receiver: Option<Receiver<TensorProto>> = None;
+        let (output_sender, output_receiver) = channel();
         let input_node_name: &String = &graph.input.get(0).unwrap().name;
         let output_node_name: &String = &graph.output.get(0).unwrap().name;
         let mut optional_receiver: Option<Receiver<TensorProto>> = None;
         for current_node in graph.node.into_iter() {
             let (sender, receiver) = channel();
+            let mut senders = Vec::new();
 
             if current_node.input.contains(&input_node_name) {
                 input_senders.push(sender.clone());
             }
 
             if current_node.output.contains(&output_node_name) {
-                output_receiver = Some(receiver);
-                optional_receiver = None;
-            } else {
-                optional_receiver = Some(receiver)
+                senders.push(output_sender.clone());
             }
+            optional_receiver = Some(receiver);
 
             //per ogni valore NodeProto creo un elemento del vettore node_io_vec
             let new_node_io = NodeIO {
-                senders: Vec::new(), //è il vettore dei sender che verrà generato in seguito dai nodi che hanno come input l'output del nodo in questione
+                senders, //è il vettore dei sender che verrà generato in seguito dai nodi che hanno come input l'output del nodo in questione
                 optional_receiver,   //receiver da cui leggere gli input
                 node: current_node.clone(),
                 initializers: get_initializers(model.clone().graph.unwrap(), current_node.clone()),
@@ -79,7 +78,7 @@ impl OnnxRunningEnvironment {
         Self {
             input_tensor,
             input_senders,
-            output_receiver: output_receiver.unwrap(),
+            output_receiver,
             model,
             node_io_vec,
         }
