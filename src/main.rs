2bcec9a37c6c89ce4be7c4fd06dfc5224403ef05
Gioeleramondetti5 <gioeleramondetti20@gmail.com>
2023-11-25 11:23:55 +0100

    inserimento nomi classi implementazione uso custom image

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,19 @@ mod onnx {
 use crate::onnx::tensor_proto::DataType;
 use crate::onnx::TensorProto;
 use crate::onnx_running_environment::OnnxRunningEnvironment;
+use crate::operations::utils::ndarray_to_tensor_proto;
 use crate::utils::get_random_float_tensor;
+use crate::utils::CLASSES_NAMES;
 use core::fmt;
+use image::GenericImage;
+use image::imageops;
+use ndarray::Array3;
 use ndarray::{arr2, s, Array, Array2, Array4, ArrayD, ArrayView, Axis, Dimension, IxDyn, Zip};
 use onnx::tensor_proto::DataLocation;
 use onnx::ModelProto;
 use operations::utils::tensor_proto_to_ndarray;
 use rand::prelude::*;
+use image::{GenericImageView, DynamicImage};
 use std::any::type_name;
 use std::error::Error;
 use std::fs::File;
@@ -26,12 +32,21 @@ mod onnx_running_environment;
 mod operations;
 mod utils;
 
+const MIN: u32 = 256;
+const CROP: u32 = 224;
+// valori standard utilizzati
+const MEAN: [f32; 3] = [0.485, 0.456, 0.406];
+const STD: [f32; 3] = [0.229, 0.224, 0.225];
+const SCALEFACTOR: f32 = 255.0;
 fn main() {
     print_results(get_random_float_tensor(vec![1, 1000, 1, 1]));
 
     let mut path_model: &str = "";
     let mut path_testset: &str = "";
     let mut path_output: &str = "";
+    let mut use_custom_img=0;
+    let mut path_img="";
+    let mut take_choice = String::new();
     loop {
         println!("Onnx runtime");
         println!("scegli una rete:");
@@ -71,7 +86,14 @@ fn main() {
                             break;
                         }
                         "n" => {
-                            println!("implementare come inserire un test set diverso");
+                            println!("inserire il path dell'immagine che si vuole utilizzare\nl'immagine va inserita in src/nomeimmagine");
+                            io::stdin()
+                                .read_line(&mut take_choice)
+                                .expect("Errore durante la lettura dell'input");
+                            // Rimuovi spazi e caratteri di nuova linea dall'input
+                            path_img = take_choice.trim();
+                            //prepar per fare conversione
+                            use_custom_img=1;
                             break;
                         }
                         _ => println!("Scelta non valida. Riprova."),
@@ -97,7 +119,14 @@ fn main() {
                             break;
                         }
                         "n" => {
-                            println!("implementare come inserire un test set diverso");
+                            println!("inserire il path dell'immagine che si vuole utilizzare\nl'immagine va inserita in src/nomeimmagine");
+                            io::stdin()
+                                .read_line(&mut take_choice)
+                                .expect("Errore durante la lettura dell'input");
+                            // Rimuovi spazi e caratteri di nuova linea dall'input
+                            path_img = take_choice.trim();
+                            //prepar per fare conversione
+                            use_custom_img=1;
                             break;
                         }
                         _ => println!("Scelta non valida. Riprova."),
@@ -123,7 +152,14 @@ fn main() {
                             break;
                         }
                         "n" => {
-                            println!("implementare come inserire un test set diverso");
+                            println!("inserire il path dell'immagine che si vuole utilizzare\nl'immagine va inserita in src/nomeimmagine");
+                            io::stdin()
+                                .read_line(&mut take_choice)
+                                .expect("Errore durante la lettura dell'input");
+                            // Rimuovi spazi e caratteri di nuova linea dall'input
+                            path_img = take_choice.trim();
+                            //prepar per fare conversione
+                            use_custom_img=1;
                             break;
                         }
                         _ => println!("Scelta non valida. Riprova."),
@@ -149,7 +185,14 @@ fn main() {
                             break;
                         }
                         "n" => {
-                            println!("implementare come inserire un test set diverso");
+                            println!("inserire il path dell'immagine che si vuole utilizzare\nl'immagine va inserita in src/nomeimmagine");
+                            io::stdin()
+                                .read_line(&mut take_choice)
+                                .expect("Errore durante la lettura dell'input");
+                            // Rimuovi spazi e caratteri di nuova linea dall'input
+                            path_img = take_choice.trim();
+                            //prepar per fare conversione
+                            use_custom_img=1;
                             break;
                         }
                         _ => println!("Scelta non valida. Riprova."),
@@ -175,7 +218,14 @@ fn main() {
                             break;
                         }
                         "n" => {
-                            println!("implementare come inserire un test set diverso");
+                            println!("inserire il path dell'immagine che si vuole utilizzare\nl'immagine va inserita in src/nomeimmagine");
+                            io::stdin()
+                                .read_line(&mut take_choice)
+                                .expect("Errore durante la lettura dell'input");
+                            // Rimuovi spazi e caratteri di nuova linea dall'input
+                            path_img = take_choice.trim();
+                            //prepar per fare conversione
+                            use_custom_img=1;
                             break;
                         }
                         _ => println!("Scelta non valida. Riprova."),
@@ -192,8 +242,27 @@ fn main() {
     }
     // Load and parse your ProtoBuf file (e.g., "squeezenet.onnx")
     //let data = std::fs::read("src/squeezenet.onnx").expect("Failed to read ProtoBuf file");
-    if path_model.is_empty() || path_testset.is_empty() {
+    if ((path_model.is_empty() || path_testset.is_empty()) && use_custom_img==0) {
         exit(1)
+    }
+    if ((path_model.is_empty() || path_testset.is_empty()) && use_custom_img==1) {
+        // uso immagine fornita da utente
+        let data = std::fs::read(path_model).expect("Failed to read ProtoBuf file");
+        let model_proto: ModelProto =
+            prost::Message::decode(&data[..]).expect("Failed to decode ProtoBuf data");
+
+        println!("Reading the inputs ...");
+        // uso immagine 
+        let arrD_img= convert_img(path_img.to_string());
+        let input_tensor: TensorProto =ndarray_to_tensor_proto::<f32>(arrD_img, "data").unwrap();
+
+        println!("starting Network...");
+        let new_env = OnnxRunningEnvironment::new(model_proto, input_tensor);
+
+        let pred_out = new_env.run(); //predicted output
+
+        println!("Predicted classes:");
+        print_results(pred_out);
     }
     let data = std::fs::read(path_model).expect("Failed to read ProtoBuf file");
     let model_proto: ModelProto =
@@ -228,7 +297,7 @@ fn print_results(tensor: TensorProto) {
         .sorted_by(|a, b| b.1.total_cmp(a.1))
         .take(3)
     {
-        print!("|Class n:{} Value:{}| ", element.0, element.1);
+        print!("|Class n:{} Value:{}| ", CLASSES_NAMES[element.0], element.1);
     }
 }
 
@@ -325,6 +394,93 @@ fn alexnet_load() -> &'static str {
 fn alexnet_load_output() -> &'static str {
     let path_output = "src/alexnet/data_alexnet/output_0.pb";
     return path_output;
+}
+fn resize_image(image: DynamicImage, width: u32, height: u32, new_width: u32, new_height: u32) -> DynamicImage {
+    // Resize the image
+    let resized_image = image.resize_exact(new_width, new_height, image::imageops::FilterType::Lanczos3);
+
+    // Create a new image with the desired dimensions (1*3*224*224)
+    let mut final_image = DynamicImage::new_rgba8(width * new_width, height * new_height);
+
+    // Paste the resized image into the final image
+    final_image.copy_from(&resized_image, 0, 0);
+
+    final_image
+}
+fn convert_img(path: String) -> ArrayD<f32> {
+    // Load the image
+    let mut img = image::open(path).unwrap();
+
+    let (width, height) = img.dimensions();
+
+    // faccio il resize a min 
+    let (nwidth, nheight) = if width > height {
+        (MIN* width / height, MIN)
+    } else {
+        (MIN,MIN* height / width)
+    };
+
+    img = img.resize(nwidth, nheight, imageops::FilterType::Gaussian);
+
+    // Crop the image to CROP_SIZE from the center
+    let crop_x = (nwidth - CROP) / 2;
+    let crop_y = (nheight - CROP) / 2;
+
+    img = img.crop_imm(crop_x, crop_y, CROP, CROP); // faccio crop a 224 come voglio input
+
+    //trasformo in rgb e poi trasformo in ndarray per utilizzarlo
+    let img_rgb = img.to_rgb8();
+
+    let raw_data = img_rgb.into_raw();
+
+    let (mut rs, mut gs, mut bs) = (Vec::new(), Vec::new(), Vec::new());
+
+    for i in 0..raw_data.len() / 3 {
+        rs.push(raw_data[3 * i]);
+        gs.push(raw_data[3 * i + 1]);
+        bs.push(raw_data[3 * i + 2]);
+    }
+
+    let r_arr: Array2<u8> =Array::from_shape_vec((CROP as usize, CROP as usize), rs).unwrap();
+    let g_arr: Array2<u8> =Array::from_shape_vec((CROP as usize, CROP as usize), gs).unwrap();
+    let b_arr: Array2<u8> =Array::from_shape_vec((CROP as usize, CROP as usize), bs).unwrap();
+    //creo Array3
+    let mut arr_fin: Array3<u8> =ndarray::stack(Axis(2), &[r_arr.view(), g_arr.view(), b_arr.view()]).unwrap();
+    //faccio trasposta
+    arr_fin.swap_axes(0, 2);
+
+    let mean = Array::from_shape_vec(
+        (3, 1, 1),
+        vec![
+            MEAN[0] * SCALEFACTOR,
+            MEAN[1] * SCALEFACTOR,
+            MEAN[2] * SCALEFACTOR,
+        ],
+    )
+    .unwrap();
+
+    let std = Array::from_shape_vec(
+        (3, 1, 1),
+        vec![
+            STD[0] * SCALEFACTOR,
+            STD[1] * SCALEFACTOR,
+            STD[2] * SCALEFACTOR,
+        ],
+    )
+    .unwrap();
+
+    let mut arr_f: Array3<f32> = arr_fin.mapv(|x| x as f32);
+
+    arr_f -= &mean;
+    arr_f /= &std;
+
+    //aggiungo la batch dim
+    let arr_f_batch: Array4<f32> = arr_f.insert_axis(Axis(0));
+
+    // Convert Array4 to ArrayD
+    let arr_d: ArrayD<f32> = arr_f_batch.into_dimensionality().unwrap();
+
+    arr_d
 }
 
 struct Operation {
