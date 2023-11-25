b52326ebd9b174f7ed1e6cfd99b1381f5e8858a0
DarioMarchitelli <d.marchitelli1@studenti.poliba.it>
2023-11-25 19:44:00 +0100

    Add dockerization

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3001;
+
+app.use(cors());
+
+app.get('/get', (req, res) => {
+  res.json({ message: 'Hello from Node.js backend!' });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
