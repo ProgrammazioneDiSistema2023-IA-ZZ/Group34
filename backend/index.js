46ee7556db40d30fc273202a0c4321211cfe5d41
DarioMarchitelli <d.marchitelli1@studenti.poliba.it>
2023-12-09 12:29:58 +0100

    fix port bindings

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = 3001;
+const port = 7777;
 
 //get rust functions
 const rust = require('.');
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ function App() {
   const [message, setMessage] = useState('');
 
   useEffect(() => {
-    fetch('/api/get')
+    fetch('http://localhost:7777/get')
       .then(response => response.json())
       .then(data => setMessage(data.message))
       .catch(error => console.error('Error:', error));
