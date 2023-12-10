472b54c62aa7863ebe64f8fd5b2a54ca9c6f8235
DarioMarchitelli <d.marchitelli1@studenti.poliba.it>
2023-12-10 14:45:12 +0100

    WIP

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ function App() {
   const [message, setMessage] = useState('');
 
   useEffect(() => {
-    fetch('http://localhost:7777/get')
+    fetch('/api/get')
       .then(response => response.json())
       .then(data => setMessage(data.message))
       .catch(error => console.error('Error:', error));
